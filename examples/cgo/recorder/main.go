757a67f422fa18cdfa729be831e73eb5c3fa4d34
Nelo-T. Wallus <nelo@wallus.de>
2019-05-16 09:03:54 +0200

    Update example recorder

--- examples/cgo/recorder/main.go
+++ examples/cgo/recorder/main.go
@@ -16,31 +16,29 @@ func main() {
 	}
 }
 
-// TODO
-
 func doMain() error {
 	dsn := libdsn.NewDsnInfoFromEnv("")
 
+	fmt.Println("Opening database")
 	db, err := sql.Open("ase", dsn.AsSimple())
 	if err != nil {
-		return err
+		return fmt.Errorf("Failed to open connection to database: %v", err)
 	}
 	defer db.Close()
 
-	recorder := ase.NewServerMessageRecorder()
-	ase.GlobalServerMessageBroker.RegisterHandler(recorder.Handle)
+	fmt.Println("Creating MessageRecorder")
+	recorder := ase.NewMessageRecorder()
+	fmt.Println("Registering handler with server message broker")
+	ase.GlobalServerMessageBroker.RegisterHandler(recorder.HandleMessage)
 
+	fmt.Println("Calling dbcc")
 	_, err = db.Exec("dbcc checkalloc")
 	if err != nil {
 		return err
 	}
 
-	_, lines, err := recorder.Text()
-	if err != nil {
-		return err
-	}
-
-	for _, line := range lines {
+	fmt.Println("Received messages:")
+	for _, line := range recorder.Text() {
 		fmt.Print(line)
 	}
 
