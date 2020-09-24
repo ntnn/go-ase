1a755796e683d257f3e1aeb806e08242c1767e4e
Nelo-T. Wallus <nelo@wallus.de>
2020-09-24 10:27:36 +0000

    Update TokenlessPackage to act as a general purpose control packge for tds

--- libase/tds/packageTokenless.go
+++ libase/tds/packageTokenless.go
@@ -9,7 +9,22 @@ import (
 	"fmt"
 )
 
+// TokenlessType defines the type of a TokenlessPackage
+type TokenlessType int
+
+const (
+	// TokenlessPackage is a data container, e.g. for login buffers
+	DataPackage TokenlessType = iota
+	// TokenlessPackage signals the end of a payload
+	EOMPackage
+)
+
+// TokenlessPackage is used to store tokenless payloads from the server
+// as well as internal communication in go-ase.
+// The type of a TokenlessPackage is defined by the TokenlessType.
 type TokenlessPackage struct {
+	Type TokenlessType
+	// Data is only valid if Type is DataPackage.
 	Data *bytes.Buffer
 }
 
@@ -20,14 +35,25 @@ func NewTokenlessPackage() *TokenlessPackage {
 }
 
 func (pkg *TokenlessPackage) ReadFrom(ch BytesChannel) error {
+	if pkg.Type != DataPackage {
+		return nil
+	}
 	_, err := pkg.Data.ReadFrom(ch)
 	return err
 }
 
 func (pkg TokenlessPackage) WriteTo(ch BytesChannel) error {
+	if pkg.Type != DataPackage {
+		return nil
+	}
 	return ch.WriteBytes(pkg.Data.Bytes())
 }
 
 func (pkg TokenlessPackage) String() string {
-	return fmt.Sprintf("%T(possibleToken=%x) %#v", pkg, pkg.Data.Bytes()[0], pkg)
+	var possibleToken byte = 0
+	if pkg.Data != nil && len(pkg.Data.Bytes()) > 0 {
+		possibleToken = pkg.Data.Bytes()[0]
+	}
+
+	return fmt.Sprintf("%T(%s, possibleToken=%x) %#v", pkg, pkg.Type, possibleToken, pkg)
 }
